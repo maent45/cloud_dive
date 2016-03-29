e985781dc3b4bc530fcb90a0d19fdb85d89f7ef7
Maen Terawasi <maent45@gmail.com>
2016-03-30 11:10:08 +1300

    cutting up site structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,33 @@
 /**
  * Created by maent45 on 29/03/16.
  */
+module.exports = function(grunt) {
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "themes/cloud-dive/css/main.css": "themes/cloud-dive/less/main.less",
+                    "themes/cloud-dive/css/test.css": "themes/cloud-dive/less/test.less" // destination file and source file
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['themes/cloud-dive/less/**/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['less', 'watch']);
+};
---
+++ themes/cloud-dive/templates/Layout/HomePage.ss
@@ -0,0 +1,1 @@
+<h2>HomePage Section</h2>
