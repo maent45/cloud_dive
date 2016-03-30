f7bfeb2888203940934721436c365277c3c83303
Maen Terawasi <maent45@gmail.com>
2016-03-30 14:36:43 +1300

    rendered form onto homepage template

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -2,8 +2,17 @@
 
 class HomePage extends Page {
 
+    // get ServicesHolder contents
     public function Services() {
         return ServicesHolder::get();
+    }
+
+    // get UserDefinedForm contents http://www.silverstripe.org/community/forums/all-other-modules/show/111503
+    public function showForm() {
+        $record = DataObject::get_one("UserDefinedForm");
+        $results = new UserDefinedForm_Controller($record);
+
+        return $results;
     }
 
 }
--- themes/cloud-dive/templates/Layout/HomePage.ss
+++ themes/cloud-dive/templates/Layout/HomePage.ss
@@ -4,8 +4,17 @@ $Content
 
 <!-- loop all services -->
 <% loop $Services %>
+
+    <!-- loop all children of services -->
     <% loop $Children %>
         $Title
         <a href="$Link">$Title</a>
     <% end_loop %>
+
 <% end_loop %>
+
+<!-- render contact form -->
+<% with $showForm %>
+    <h1>Contact Form</h1>
+    $Form
+<% end_with %>
