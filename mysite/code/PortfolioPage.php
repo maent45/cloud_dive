ed8f6c05d66962af6a5b9c493083e81d26aa2ae1
Maen Terawasi <maent45@gmail.com>
2016-04-08 03:53:17 +1200

    more db fields for portfolio_page

--- mysite/code/PortfolioPage.php
+++ mysite/code/PortfolioPage.php
@@ -4,11 +4,16 @@ class PortfolioPage extends Page {
 
     private static $db = array (
         'Content' => 'HTMLText',
-        'Blurb' => 'HTMLText'
+        'Blurb' => 'HTMLText',
+        // main content
+        'Tablet_desc' => 'HTMLText',
+        'Desktop_desc' => 'HTMLText'
     );
 
     private static $has_one = array (
-        'Thumbnail_background' => 'Image'
+        'Thumbnail_background' => 'Image',
+        'Tablet_view' => 'Image',
+        'Desktop_view' => 'Image'
     );
 
     private static $can_be_root = false;
@@ -18,13 +23,22 @@ class PortfolioPage extends Page {
 
         $fields->addFieldToTab('Root.Main', $thumbnail_bkground = UploadField::create('Thumbnail_background', 'Thumbnail Background'));
         $fields->addFieldToTab('Root.Main', $blurb = HtmlEditorField::create('Blurb')->setRows(4));
+        // main content areas
+        $fields->addFieldToTab('Root.Main', HeaderField::create('Main Content'));
         $fields->addFieldToTab('Root.Main', HtmlEditorField::create('Content'));
+        $fields->addFieldToTab('Root.Main', HtmlEditorField::create('Tablet_desc', 'Development Summary')->setRows(5));
+        $fields->addFieldToTab('Root.Main', $tablet_img = UploadField::create('Tablet_view', 'Tablet Image'));
+        $fields->addFieldToTab('Root.Main', HtmlEditorField::create('Desktop_desc', 'Final Product Summary')->setRows(5));
+        $fields->addFieldToTab('Root.Main', $desktop_img = UploadField::create('Desktop_view', 'Tablet Image'));
 
         // set upload folder for images and files
         $thumbnail_bkground->setFolderName('portfolio-thumbnail-images');
-
+        $tablet_img->setFolderName('portfolio-images');
+        $desktop_img->setFolderName('portfolio-images');
         // set validators
         $thumbnail_bkground->getValidator()->setAllowedExtensions(array('png','gif','jpeg','jpg','svg'));
+        $tablet_img->getValidator()->setAllowedExtensions(array('png','gif','jpeg','jpg','svg'));
+        $desktop_img->getValidator()->setAllowedExtensions(array('png','gif','jpeg','jpg','svg'));
 
         return $fields;
     }
