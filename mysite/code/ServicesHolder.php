f7a9a634a759cb24d04d73960651b482ff6e1ce7
Maen Terawasi <maent45@gmail.com>
2016-04-07 17:22:17 +1200

    services_holder ui update

--- mysite/code/ServicesHolder.php
+++ mysite/code/ServicesHolder.php
@@ -6,6 +6,10 @@ class ServicesHolder extends Page {
         'Homepage_display_limit' => 'Varchar'
     );
 
+    private static $has_one = array (
+        'Banner' => 'Image'
+    );
+
     private static $allowed_children = array (
         'ServicesPage'
     );
@@ -16,6 +20,11 @@ class ServicesHolder extends Page {
         $children_count = $this->Children()->count();
 
         $fields->addFieldToTab('Root.Main', TextField::create('Homepage_display_limit', 'Homepage Display Limit'), 'Content');
+        $fields->addFieldToTab('Root.Main', $banner = UploadField::create('Banner'), 'Content');
+
+        $banner->setFolderName('service-holder-images');
+
+        $banner->getValidator()->setAllowedExtensions(array('png','gif','jpeg','jpg','svg'));
 
         return $fields;
     }
--- themes/cloud-dive/templates/Layout/ServicesHolder.ss
+++ themes/cloud-dive/templates/Layout/ServicesHolder.ss
@@ -1,3 +1,20 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 banner_holder">
+    <div class="container banner_content">
+        <h1>$Title</h1>
+        <p>$Blurb</p>
+        <a href="contact-us">
+            <button>
+                <span>CONTACT US NOW</span>
+                <div class="btn_bground"></div>
+            </button>
+        </a>
+    </div>
+    <div class="img_filter"></div>
+    <% if $Banner %>
+        <img src="$Banner.URL"/>
+    <% end_if %>
+</div>
+
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
     $Content
 </div>
