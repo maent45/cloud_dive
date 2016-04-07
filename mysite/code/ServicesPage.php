029a98b7951258ac7faddf185b1b227d6bbd918c
Maen Terawasi <maent45@gmail.com>
2016-04-07 14:20:14 +1200

    update for services_page UI

--- mysite/code/ServicesPage.php
+++ mysite/code/ServicesPage.php
@@ -4,7 +4,12 @@ class ServicesPage extends Page {
 
     private static $db = array (
         'Content' => 'HTMLText',
-        'Glyphicon' => 'Varchar'
+        'Glyphicon' => 'Varchar',
+        'Blurb' => 'Text'
+    );
+
+    private static $has_one = array (
+        'Banner' => 'Image'
     );
 
     private static $can_be_root = false;
@@ -14,7 +19,15 @@ class ServicesPage extends Page {
         $fields = parent::getCMSFields();
 
         $fields->addFieldToTab('Root.Main', TextField::create('Glyphicon'), 'Content');
+        $fields->addFieldToTab('Root.Main', $service_banner = UploadField::create('Banner'), 'Content');
+        $fields->addFieldToTab('Root.Main', TextareaField::create('Blurb'), 'Content');
         $fields->addFieldToTab('Root.Main', HtmlEditorField::create('Content'));
+
+        // set upload folder for images and files
+        $service_banner->setFolderName('service_banner-images');
+
+        // set validators
+        $service_banner->getValidator()->setAllowedExtensions(array('png','gif','jpeg','jpg','svg'));
 
         return $fields;
 
--- themes/cloud-dive/templates/Layout/ServicesPage.ss
+++ themes/cloud-dive/templates/Layout/ServicesPage.ss
@@ -1,4 +1,12 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 banner_holder">
+    <div class="container banner_content">
+        <h1>$Title</h1>
+        <p>$Blurb</p>
+    </div>
+    <div class="img_filter"></div>
+    <img src="$Banner.URL"/>
+</div>
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <h1>$Title</h1>
+    $Content
 </div>
-$Content
